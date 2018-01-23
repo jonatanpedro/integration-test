9e335c1443d11e31ef16a479bf2316e4b18e8764
Jonatan Pedro da Silva <jonatanpedro@gmail.com>
2018-01-23 15:00:41 -0200

    Implementation on flowService handler.

--- src/main/java/com/example/integrationtest/service/FlowService.java
+++ src/main/java/com/example/integrationtest/service/FlowService.java
@@ -1,13 +1,13 @@
 package com.example.integrationtest.service;
 
 import com.example.integrationtest.dto.Flow;
-import com.example.integrationtest.flow.ConsoleMessagehandler;
-import com.example.integrationtest.flow.JdbcGenericMessageHandler;
-import com.example.integrationtest.flow.MessageHandlerFactory;
-import com.example.integrationtest.flow.TransformerFactory;
+import com.example.integrationtest.flow.*;
 import com.example.integrationtest.repository.FlowRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationContext;
 import org.springframework.http.HttpMethod;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlowBuilder;
@@ -15,12 +15,16 @@ import org.springframework.integration.dsl.IntegrationFlows;
 import org.springframework.integration.http.dsl.Http;
 import org.springframework.integration.jdbc.JdbcMessageHandler;
 import org.springframework.integration.json.JsonToObjectTransformer;
+import org.springframework.integration.support.MessageBuilder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.WebApplicationContext;
 
 import javax.sql.DataSource;
 
 @Service
 public class FlowService {
+
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     private FlowRepository repository;
@@ -35,42 +39,56 @@ public class FlowService {
     @Autowired
     private DataSource dataSource;
 
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Autowired
+    private IntegrationGateway gateway;
+
     public void createFlow(Flow flow){
-        repository.save(flow);
+        try {
+            repository.save(flow);
+
+            boolean hasDataChanged = flow.getSourceMap()
+                    .entrySet()
+                    .stream()
+                    .anyMatch(entry -> entry.getValue().getDestination() != null);
+
+            IntegrationFlowBuilder intFlowBuilder = IntegrationFlows.from("channel");
+
+            intFlowBuilder = intFlowBuilder
+                    .handle(Http.outboundGateway(flow.getUrlBase())
+                            .charset("UTF-8")
+                            .httpMethod(HttpMethod.GET)
+                            .expectedResponseType(String.class));
+
+            intFlowBuilder = intFlowBuilder.transform(new JsonToObjectTransformer());
+
+            if(hasDataChanged){
+                intFlowBuilder = intFlowBuilder.transform(transformerFactory.createTransformer(RestMapperTransformationService.class));
+            }
+
+            if(flow.getUseTable()){
+                intFlowBuilder = intFlowBuilder.handle(new JdbcGenericMessageHandler(flow, dataSource));
+            }
+
+            if(flow.getUseConsole()){
+                intFlowBuilder = intFlowBuilder.handle(messageHandlerFactory.createMessageHandler(ConsoleMessagehandler.class));
+            }
+
+            applicationContext.getAutowireCapableBeanFactory().autowireBean(intFlowBuilder.get());
+
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        }
     }
 
-    public IntegrationFlow executeFlow(String id){
+    public String executeFlow(String id){
 
         Flow flow = repository.findById(id).orElse(null);
 
-        boolean hasDataChanged = flow.getSourceMap()
-                .entrySet()
-                .stream()
-                .anyMatch(entry -> entry.getValue().getDestination() != null);
+        gateway.execute(flow.getId());
 
-
-        IntegrationFlowBuilder intFlowBuilder = IntegrationFlows.from("channel");
-
-        intFlowBuilder = intFlowBuilder
-                .handle(Http.outboundGateway(flow.getUrlBase())
-                .charset("UTF-8")
-                .httpMethod(HttpMethod.GET)
-                .expectedResponseType(String.class));
-
-        intFlowBuilder = intFlowBuilder.transform(new JsonToObjectTransformer());
-
-        if(hasDataChanged){
-            intFlowBuilder = intFlowBuilder.transform(transformerFactory.createTransformer(RestMapperTransformationService.class));
-        }
-
-        if(flow.getUseTable()){
-            intFlowBuilder = intFlowBuilder.handle(new JdbcGenericMessageHandler(flow, dataSource));
-        }
-
-        if(flow.getUseConsole()){
-            intFlowBuilder = intFlowBuilder.handle(messageHandlerFactory.createMessageHandler(ConsoleMessagehandler.class));
-        }
-
-        return intFlowBuilder.get();
+        return null;
     }
 }
